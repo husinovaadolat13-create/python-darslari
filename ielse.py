1cb618ba9b778733343141c4c91f9afc934d44d4
isroil <husinovaadolat13@gmail.com>
2025-10-28 17:24:05 +0500

    Create ielse.py

---
+++ ielse.py
@@ -0,0 +1,145 @@
+# masala  10
+# a = int(input("a = "))
+# b = int(input("b = "))
+# if a != b:
+#        yigindi = a + b
+#        a = yigindi
+#        b = yigindi
+# else:
+#    a = 0
+# b = 0 
+# print(f"a = {a}, b = {b} ")      
+
+
+
+
+# masala 11
+# a = int(input("a = "))
+# b = int(input("b = "))
+# if a == b:
+#    a = 0 
+#    b = 0
+# else:
+#      if a > b:
+#       b = a
+#      else :
+#          a = b
+# print(f"a = {a}, b = {b}")                 
+
+
+
+# masala 12
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# if a > b > c:
+#     print("Eng kichik son:", c)
+# elif b > c > a:
+#     print("Eng kichik son:", a)
+# elif a > c > b:
+#     print("Eng kichik son:", b)
+# else:
+#     print(f"a = {a}, b= {b}, c = {c}")
+
+
+
+# masala 15
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))  
+# yig1 = a + b
+# yig2 = a + c
+# yig3 = b + c
+# if yig1 >= yig2 and yig1 >= yig3:
+#     print("Eng katta yigindi:", yig1, "(a + b)")
+# elif yig2 >= yig1 and yig2 >= yig3:
+#     print("Eng katta yigindi:", yig2, "(a + c)")
+# else:
+#     print("Eng katta yigindi:", yig3, "(b + c)")
+
+
+
+# masala 16
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# if a < b < c:
+#     a = 2 * a
+#     b = 2 * b
+#     c = 2 * c
+# else:a = -a
+# b = -b
+# c = -c
+# print(f"sonlar: {a, b, c}")
+
+
+
+# masala 17
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# if a < b < c :
+#     a = 2 * a
+#     b = 2 * b
+#     c = 2 * c
+#     print(f"sonlar osish tartibida: {a, b, c}")
+# elif a > b > c:
+#   a = 2 * a
+#   b = 2 * b
+#   c = 2 * c
+#   print(f"sonlar kamayish tartibida:  {a, b, c}")
+# else: a = -a
+# b = -b
+# c = -c
+# print(f"son: {a, b, c}")
+ 
+
+
+# masala 18
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# if a == b:
+#    print(3)   
+
+# elif  a == c : 
+#   print(2)
+# else: 
+#    print(1) 
+
+
+
+
+# masala 19
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# d = float(input("d = "))
+# if a == b:
+#    print(4)   
+
+# elif  a == c : 
+#   print(3)
+# elif a == d : 
+#    print(2)
+# else: 
+#    print(1)
+
+
+
+
+# masala 20
+
+
+
+lkjhikhjk
+
+
+
+
+
+
+
+
+
+
